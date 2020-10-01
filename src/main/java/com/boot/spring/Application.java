8eaac33c35bdcd09272a2a021e42a8eaed65a0a6
nisum <nisum@Kiran-Ramreddy.local>
2020-10-01 11:02:53 +0530

    first commit

---
+++ src/main/java/com/boot/spring/Application.java
@@ -0,0 +1,14 @@
+package com.boot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Spring boot sample app");
+	}
+
+}
